e59d27341dc9c370557cb4433a6df7ddefef7270
shettyabhijith <65816249+shettyabhijith@users.noreply.github.com>
2021-02-11 13:31:45 +0530

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -80,19 +80,57 @@ const liveUpdate = async() => {
             </div>
         </div>
 
-
     `;
 
     
 
 
 
+  var  country =  data["Countries"];
 
-console.log(global["Date"]);
+console.log(data);
+
+    var slNo = 1;
+    var countryHtml = "";
+
+    for(var i=0; i<country.length ; i++){
+        const { Country, NewConfirmed, NewDeaths, NewRecovered, TotalConfirmed, TotalDeaths, TotalRecovered} = country[i];
+
+        
+        countryHtml += `
+        <tr>
+            <td><b>${slNo}</b></td>
+            <td>${Country}</td>
+            <td>${NewConfirmed}</td>
+            <td>${NewDeaths}</td>
+            <td>${NewRecovered}</td>
+            <td>${TotalConfirmed}</td>
+            <td>${TotalDeaths}</td>
+            <td>${TotalRecovered}</td>
+        </tr>
+        `;
+
+        slNo++;
+    }
 
 
+
+    var countryHeader = `<tr>
+                            <th colspan="8" style="text-align:center; background-color:#5E1D55;color:white;"><h3>Overview</h3></th>
+                        </tr>
+
+                        <tr style="background-color:#32A9EE;color:white;">
+                            <th>Sl. No.</th>
+                            <th>Country</th>
+                            <th>NewConfirmed</th>
+                            <th>NewDeaths</th>
+                            <th>NewRecovered</th>
+                            <th>TotalConfirmed</th>
+                            <th>TotalDeaths</th>
+                            <th>TotalRecovered</th>
+                        </tr>`;
 
     document.getElementById("statusLiveUpdate").innerHTML = myHtml;
-
+    document.getElementById("countryOverviewDetails").innerHTML = countryHeader + countryHtml;
 }
 
